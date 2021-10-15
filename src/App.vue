6739c0241e5f40790e946803b5017ea214899675
jpx <1074504362@qq.com>
2021-10-15 10:19:26 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
+//每个vue组件，由三部分构成：template、script、style
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="top">
+    <div>请输入用户名<input type="text" v-model="name" /></div>
+    <div>
+      <button @click="isShowName()">点击显示用户名</button>
+      <p v-if="isShow">你的用户名是：{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  //vue组件中的data只能定义成函数形式,且数据在return中被定义
+  data() {
+    return {
+      name: "蒋沛汛",
+      isShow: false,
+    };
+  },
+
+  //方法
+  methods: {
+    isShowName() {
+      if (this.isShow) {
+        this.isShow = false;
+      } else {
+        this.isShow = true;
+      }
+    },
+  },
+
+  //侦听器
+  watch:{},
+  //计算属性
+  computed:{},
+  //过滤器
+  filters:{}
+};
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style lang='less'>
+.top {
+  background-color: pink;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+//导入Vue包，得到Vue构造函数
 import Vue from 'vue'
+//导入App.vue跟组件
 import App from './App.vue'
+//导入test.vue
+import test from './test.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+//.$mount('#app')和el:'#app'一样
+
+new Vue({
+  render: h => h(test),
+}).$mount('#test')
+//.$mount('#app')和el:'#app'一样
